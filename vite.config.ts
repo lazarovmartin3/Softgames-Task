804094c84cb736460f61b62beea58243e03267e7
lazarovmartin3 <lazarovmartin3@gmail.com>
2025-11-06 17:11:06 +0200

    Fix base + relative asset paths for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,8 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/Softgames-Task/',
-  server: {
-    port: 3000,
-    proxy: {
-    },
-  },
   publicDir: 'public',
-  build: {
-    outDir: 'dist',
-  },
+  build: { outDir: 'dist' },
   optimizeDeps: {
     include: ['@pixi/app', '@pixi/assets', '@pixi/core', '@pixi/display', '@pixi/graphics', '@pixi/sprite', '@pixi/text'],
     exclude: ['@pixi-spine/loader-4.1', '@pixi-spine/runtime-4.1']
